2ebf431ded544559d0a32f1efe0681e6d64fa003
ntdesmond <ntdesmond@gmail.com>
2021-03-02 10:18:24 +0300

    prevent interrupting cuurrent message by incoming ones

--- lab7/udp_chat.c
+++ lab7/udp_chat.c
@@ -6,15 +6,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h> // for inet_ntop()
-#define BUF_SIZE 1024
+#define BUF_SIZE 1000
 #define MAX_CLIENTS 100
+#define ADDR_LEN 24 // len("xxx.xxx.xxx.xxx:nnnnn > ")
+
+const char prompt[] =  "chat> ";
 
 // Passing args to pthread using this struct. See message_receiver(void*)
 struct msg_receiver_args {
-    int socket;
-    struct sockaddr* sa;
+    int socket;              // socket to use
+    struct sockaddr* sa;     // client info to use with recvfrom
     socklen_t* sa_size;
-    int* ret_value;
+    char* typed_message;     // the part of message typed by client, but not sent yet
+    int* ret_value;          // the return value of the thread
 };
 
 
@@ -50,7 +54,12 @@ void* message_receiver(void* args) {
             return NULL;
         }
         else {
-             printf("%s", response);
+             printf("\r%s", response);                                                   // \r would overwrite the message on edit
+             int diff = strlen(prompt) + strlen(((struct msg_receiver_args*)args)->typed_message) - strlen(response);
+             for (int i = 0; i < diff; i++)
+                 printf(" ");                                                            // so we erase it to it's full length   
+             printf("\n%s%s", prompt, ((struct msg_receiver_args*)args)->typed_message); // and restore it afterwards
+             fflush(stdout);
         }
     }
 }
@@ -109,7 +118,7 @@ int main(int argc, char* argv[]) {
         for (;;) {
             struct sockaddr_in sa_client;
             socklen_t sa_size = sizeof(sa_client);
-            char client_addr[30] = "<unknown IP>";
+            char client_addr[ADDR_LEN] = "<unknown IP>";
 
             char recv_buffer[BUF_SIZE] = "";
             if (recvfrom(server_socket, recv_buffer, BUF_SIZE, 0, (struct sockaddr*)&sa_client, &sa_size) == -1) {
@@ -128,19 +137,18 @@ int main(int argc, char* argv[]) {
                 // Get the IP
                 inet_ntop(AF_INET, &(sa_client.sin_addr), client_addr, INET_ADDRSTRLEN);
                 if (show_client_port_param > 0) {
-                    // Get the port
-                    char port_str[6];
-                    snprintf(port_str, 6, ":%d", ntohs(sa_client.sin_port));
-                    strcat(client_addr, port_str);
+                    // Get the port (snprintf for quick int to str conversion)
+                    snprintf(client_addr+strlen(client_addr), 6, ":%d", ntohs(sa_client.sin_port));
                 }
                 strcat(client_addr, "> ");
-                strcat(recv_buffer, "\n");
                 
-                printf("%s%s", client_addr, recv_buffer);
+                char send_buffer[ADDR_LEN + BUF_SIZE] = "";
+                strcat(send_buffer, client_addr);
+                strcat(send_buffer, recv_buffer);
+                printf("%s\n", send_buffer);
                 // Send the message to every client
                 for (int i = 0; i < clients_known; i++) {
-                    sendto(server_socket, client_addr, strlen(client_addr), 0, (struct sockaddr*)&clients[i], sizeof(clients[i]));
-                    sendto(server_socket, recv_buffer, strlen(recv_buffer), 0, (struct sockaddr*)&clients[i], sizeof(clients[i]));
+                    sendto(server_socket, send_buffer, strlen(send_buffer), 0, (struct sockaddr*)&clients[i], sizeof(clients[i]));
                 }
             }
         }
@@ -148,7 +156,7 @@ int main(int argc, char* argv[]) {
         close(server_socket);
     }
     else {
-        printf("Client mode; server == %s:%d\n\n", server_addr, server_port);
+        printf("Client mode; server == %s:%d\n\n%s", server_addr, server_port, prompt);
 
         struct sockaddr_in sa;
         socklen_t sa_size = sizeof(sa);
@@ -163,6 +171,8 @@ int main(int argc, char* argv[]) {
             printf("Failed to create the client socket\n");
             return 1;
         }
+
+        char request[BUF_SIZE] = "", response[BUF_SIZE] = "";
         
         // Start getting incoming messages
         pthread_t tid;
@@ -171,6 +181,7 @@ int main(int argc, char* argv[]) {
         receiver_args.socket = client_socket;
         receiver_args.sa = (struct sockaddr*)&sa;
         receiver_args.sa_size = &sa_size;
+        receiver_args.typed_message = request;
         receiver_args.ret_value = &receiver_status;
 
         if (pthread_create(&tid, NULL, &message_receiver, (void*)&receiver_args) != 0) {
@@ -179,14 +190,28 @@ int main(int argc, char* argv[]) {
         }
 
         // Send messages
+        system("stty -icanon min 1");                              // to read stdin without waiting for '\n'
         for (;;) {
             if (receiver_status != 0) // Error in the message receiver
                 return 1;
-            char request[BUF_SIZE] = "", response[BUF_SIZE] = "";
-            fgets(request, BUF_SIZE, stdin);
-            request[strcspn(request, "\n")] = 0; // remove trailing "\n"
+            memset(request, 0, strlen(request));                   // Clean the message
+            while (strlen(request) < BUF_SIZE && request[strlen(request) - 1] != '\n') {
+                char c = getchar();                                // Read char by char from stdin
+                if (c == 8 || c == 127) {                          // backspace or del character (yes, now this is our burden)
+                    if (strlen(request) > 0) {
+                        printf("\b\b\b   \b\b\b");                 // erase "C^?" (replace char C with a space)
+                        request[strlen(request) - 1] = 0;          // remove the last character from the string
+                    }
+                    else {
+                        printf("\b\b  \b\b");                      // erase "^?" only
+                    } 
+                }
+                else {
+                    request[strlen(request)] = c;
+                }
+            }
             if (strlen(request) > 0)
-                sendto(client_socket, request, strlen(request), 0, (struct sockaddr*)&sa, sa_size);
+                sendto(client_socket, request, strlen(request) - 1, 0, (struct sockaddr*)&sa, sa_size); // -1 from length to trim '\n'
         }
         close(client_socket);
     }
